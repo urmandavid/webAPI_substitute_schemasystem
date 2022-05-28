6aa896311c89d3007400fc8bb6fc5445a83b9386
urmandavid <davurm0211@skola.goteborg.se>
2022-05-28 12:58:54 +0200

    sub-task 6

--- www/WEB-INF/classes/se/yrgo/schedule/JsonFormatter.java
+++ www/WEB-INF/classes/se/yrgo/schedule/JsonFormatter.java
@@ -1,6 +1,7 @@
 package se.yrgo.schedule;
 
 import java.util.List;
+import org.json.*;
 
 /**
  * A class implementing the Formatter interface.
@@ -8,29 +9,39 @@ import java.util.List;
  */
 public class JsonFormatter implements Formatter {
     public String format(List<Assignment> assignments) {
-        return "[ { \"some-key\": \"some-value\" } ]";
+        if (assignments.size() == 0) {
+            return "[]";
+        } else {
+            JSONArray JSON = new JSONArray();
+            for (Assignment assignment : assignments) {
+                JSON.put(JSONAssignment(assignment));
+            }
+            return JSON.toString(2);
+        }
+    }
+
+    private JSONObject JSONAssignment(Assignment assignment) {
+        // Create a new empty JSONObject called JSONAssignment
+        JSONObject JSONAssignment = new JSONObject();
+        // Put the key "date" in that object, and the value from the assignment's date
+        JSONAssignment.put("date", assignment.date());
+
+        // Create a new empty JSONObject called JSONSubstitute
+        JSONObject JSONSubstitute = new JSONObject();
+        // Put the key "name" in that object, and the value from the assignment's teacher's name
+        JSONSubstitute.put("name", assignment.teacher());
+        // Put the JSONSubstitute object in the JSONAssignment object with the key "substitute"
+        JSONAssignment.put("substitute", JSONSubstitute);
+
+        // Create a new JSONObject called JSONSchool
+        JSONObject JSONSchool = new JSONObject();
+        // Put the key "school_name" in that object and the value from the assignment's school's name
+        // Put the key "address" in that object and the value from the assignment's school's address
+        JSONSchool.put("school_name", assignment.school())
+                .put("address", assignment.school());
+        // Put the JSONSchool in the JSONAssignment object with the key "school"
+        JSONAssignment.put("school", JSONSchool);
+
+        return JSONAssignment;
     }
 }
-
-/* FOR HtmlFormatter.java:
-
-        StringBuffer html = new StringBuffer("<!DOCTYPE html>\n")
-                .append("<html lang=\"en\">\n")
-                .append("<head><title>Substitutes R Us - Schedule API</title></head>\n")
-                .append("<body>\n")
-                .append("<h1>Schedule</h1>\n");
-        if (assignments.size() == 0) {
-            html.append("<em>No assignments found.</em>");
-        } else {
-            html.append("<table border=\"1\">\n <tr>\n  <th>Teacher</th>\n  <th>date</th>\n  <th>school</th>\n </tr>\n");
-            for (Assignment assignment : assignments) {
-                html.append(" <tr>\n  <td>").append(assignment.teacher()).append("</td>\n")
-                        .append("  <td>").append(assignment.date()).append("</td>\n")
-                        .append("  <td>").append(assignment.school()).append("</td>\n </tr>\n");
-            }
-            html.append("</table>\n");
-        }
-        html.append("</body>\n");
-        html.append("</html>\n");
-        return html.toString();
-        */
