8918c407f80bc402b18f828ef55dad3d011bf65a
urmandavid <davurm0211@skola.goteborg.se>
2022-05-29 20:30:38 +0200

    fixing compile script

--- compile_servlet_and_start_winstone.sh
+++ compile_servlet_and_start_winstone.sh
@@ -5,4 +5,5 @@ if [[ $OS == "Windows_NT" ]] || [[ $OSTYPE == "cygwin" ]]; then
   PATHSEP=";"
 fi
 
-javac -cp "./www/WEB-INF/classes/${PATHSEP}winstone.jar" www/WEB-INF/classes/se/yrgo/schedule/*.java && java -jar winstone.jar --webroot=www
+#javac -cp "./www/WEB-INF/classes/${PATHSEP}winstone.jar" www/WEB-INF/classes/se/yrgo/schedule/*.java && java -jar winstone.jar --webroot=www
+javac -cp ".${PATHSEP}www/WEB-INF/lib/org.json.jar${PATHSEP}winstone.jar${PATHSEP}www/WEB-INF/classes" www/WEB-INF/classes/se/yrgo/schedule/*.java && java -jar winstone.jar --webroot=www
